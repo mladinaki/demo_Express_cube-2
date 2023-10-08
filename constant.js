c56fa0636109293846b0e3590c9df2bea08b044f
mladinaki <mladinaki54@gmail.com>
2023-10-08 20:51:02 +0300

    extract connection string in the config,connect app to the db

--- constant.js
+++ constant.js
@@ -1,5 +1,6 @@
 const constants = {
     PORT: 4000,
+    URL: 'mongodb://127.0.0.1:27017/Cubical-sep-2023',
 }
 
-module.exports = constants
+module.exports = constants;
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+
 const { PORT } = require('./constant');
+
 const expressConfig = require('./src/config/expressConfig');
 const handlebarsConfig = require('./src/config/handlebarsConfig');
+const dbConnect = require('./src/config/dbConfig')
 const routers = require('./router');
+
 const app = express();
 
+//Config
 expressConfig(app);
-handlebarsConfig(app)
+handlebarsConfig(app);
+
+//Connecting to the database
+dbConnect()
+    .then(() => console.log('Successfully connected to the DB!'))
+    .catch(err => console.log(`Error while connecting to DB!!!:, ${err}`));
 
 app.use(routers);
 
