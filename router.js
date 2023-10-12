e05ea21c95c1f7108c6a97fb896d40ad96212ea8
mladinaki <mladinaki54@gmail.com>
2023-10-13 00:28:22 +0300

    initialised user model structure

--- router.js
+++ router.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const homeControler = require('./src/controllers/homeControler');
 const cubeControler = require('./src/controllers/cubeControler');
 const accessoriesControler = require('./src/controllers/accessoryControler');
+const usersControler = require('./src/controllers/userControler')
 
 router.use(homeControler);
 router.use('/cubes', cubeControler);
 router.use('/accessories',accessoriesControler);
+router.use('/users', usersControler);
 
 router.get('*', (req, res)=> {
     res.redirect('/404')
---
+++ src/controllers/userControler.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+router.get('/register', (req, res) => {
+    res.render('user/register')
+})
+
+router.post('/register', (req, res) => {
+    console.log({ input: req.body });
+    const {username, password,repeatPassword} = req.body
+    res.redirect('/users/login')
+})
+
+router.get('/login', (req, res) => {
+    res.render('user/login')
+})
+
+module.exports = router
