a9d95b58028e4c3766eca382bbba45a7b4828738
mladinaki <mladinaki54@gmail.com>
2023-10-13 12:37:15 +0300

    implement logic with bcrypt comparing

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -1,17 +1,27 @@
 const router = require('express').Router();
+const userService = require('../services/userServices')
 
 router.get('/register', (req, res) => {
     res.render('user/register')
 })
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     console.log({ input: req.body });
-    const {username, password,repeatPassword} = req.body
+    const { username, password, repeatPassword } = req.body;
+    await userService.register({ username, password, repeatPassword })
     res.redirect('/users/login')
 })
 
 router.get('/login', (req, res) => {
     res.render('user/login')
+})
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await userService.login(username, password);
+
+    console.log({ user });
+    res.redirect('/')
 })
 
 module.exports = router
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,3 +1,24 @@
-exports.register =(username,password,repeatPassword) => {
+const Users = require('../models/User');
+const bcrypt = require('bcrypt');
 
+//Create Register
+exports.register = (userData) => Users.create(userData);
+
+exports.login = async (username, password) => {
+    const user = await Users.findOne({ username });
+
+    //Validate user
+    if (!user) {
+        throw new Error('Cannot username or password');
+    }
+
+    //Validate password
+    const isValid = await bcrypt.compare(password, user.password);
+    console.log({ isValid });
+
+    if (!isValid) {
+        throw new Error('Cannot username or password!!!');
+    }
+    return user;
 }
+
